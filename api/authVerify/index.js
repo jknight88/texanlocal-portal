a934b7ea33bdf3d9682c3daad3b224886b47195e
jknight88 <josh@thetexanlocal.com>
2026-06-16 14:15:38 -0500

    Update index.js

--- api/authVerify/index.js
+++ api/authVerify/index.js
@@ -1,6 +1,8 @@
 module.exports = function(context, req) {
   context.res = {
     status: 200,
-    body: "API OK"
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ ok: true, message: "API is working!" })
   };
+  context.done();
 };
